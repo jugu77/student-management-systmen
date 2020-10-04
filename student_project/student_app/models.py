18cd6bdcf134c23e3e39b047201fce5ff743ce87
julioguzman <julio_guzman@ymail.com>
2020-10-04 13:07:56 -0600

    10/04/2020
    Nothing new on python code, working on back-end

--- student_project/student_app/models.py
+++ student_project/student_app/models.py
@@ -6,8 +6,8 @@ class AdminHOD(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
 
 class Staff(models.Model):
@@ -16,15 +16,15 @@ class Staff(models.Model):
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     address = models.TextField()
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 class Courses(models.Model):
     id = models.AutoField(primary_key=True)
     course_name = models.CharField(max_length=255)
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 class Subject(models.Model):
@@ -32,8 +32,8 @@ class Subject(models.Model):
     subject_name = models.CharField(max_length=255)
     course_id = models.ForeignKey(Courses,on_delete=models.CASCADE)
     staff_id = models.ForeignKey(Staff,on_delete=models.CASCADE)
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 class Students(models.Model):
@@ -45,16 +45,16 @@ class Students(models.Model):
     profile_pic = models.FileField()
     address = models.TextField()
     course_id = models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
 
 class Attendance(models.Model):
     id = models.AutoField(primary_key=True)
     subject_id = models.ForeignKey(Subject,on_delete=models.DO_NOTHING)
-    attendance_date = models.DateTimeFields(auto_now_add=True)
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    attendance_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
 
 class AttendanceReport(models.Model):
@@ -62,8 +62,8 @@ class AttendanceReport(models.Model):
     student_id = models.ForeignKey(Students,on_delete=models.DO_NOTHING)
     attendance_id = models.ForeignKey(Attendance,on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
 
 class LeaveReportStudent(models.Model):
@@ -72,8 +72,8 @@ class LeaveReportStudent(models.Model):
     leave_date = models.CharField(max_length=255)
     leave_message = models.TextField()
     leave_status = models.BooleanField(default=False)
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
 
 class LeaveReportStaff(models.Model):
@@ -82,8 +82,8 @@ class LeaveReportStaff(models.Model):
     leave_date = models.CharField(max_length=255)
     leave_message = models.TextField()
     leave_status = models.BooleanField(default=False)
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
 
 class FeedBackStudents(models.Model):
@@ -91,8 +91,8 @@ class FeedBackStudents(models.Model):
     student_id = models.ForeignKey(Students,on_delete=models.CASCADE)
     feedback = models.TextField()
     feedback_reply = models.TextField()
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
 
 class FeedBackStaff(models.Model):
@@ -100,22 +100,22 @@ class FeedBackStaff(models.Model):
     staff_id = models.ForeignKey(Staff,on_delete=models.CASCADE)
     feedback = models.TextField()
     feedback_reply = models.TextField()
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
 
 class NotificationStudent(models.Model):
     id = models.AutoField(primary_key=True)
     student_id = models.ForeignKey(Students,on_delete=models.CASCADE)
     message = models.TextField()
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
 
 class NotificationStaff(models.Model):
     id = models.AutoField(primary_key=True)
     staff_id = models.ForeignKey(Staff,on_delete=models.CASCADE)
     message = models.TextField()
-    created_at = models.DateTimeFields(auto_now_add=True)
-    updated_at = models.DateTimeFields(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     object = models.Manager()
